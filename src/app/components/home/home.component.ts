414d7f7ca03f654214ad002c898db736b18fe856
jcalexander <chrisalexander55@gmail.com>
2019-03-18 07:50:18 -0400

    added simple libjs test; libjs on root directory

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CasinocoinAPI} from '@casinocoin/libjs';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,20 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onTestCSCConn(e: Event) {
+    // connect to casinocoind
+    const api = new CasinocoinAPI({
+      server: 'wss://moon.casinocoin.eu:6006'
+    });
+    api.connect().then(() => {
+      console.log('connected');
+      return api.connection.request({
+        command: 'test_command',
+        data: { openOnOtherPort: true },
+      });
+    });
   }
 
 }
